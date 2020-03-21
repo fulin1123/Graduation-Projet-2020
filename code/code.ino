0b78cf7b675fdedd5c335001f2a61a490fa80936
fulin1123 <59222268+fulin1123@users.noreply.github.com>
2020-03-21 12:11:00 +0800

    调用can

--- code/code.ino
+++ code/code.ino
@@ -1,9 +1,11 @@
+#include"robomodule_due_CAN.h"
+CRobomodule_due_CAN can;
 void setup() {
-  // put your setup code here, to run once:
+  
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 
 }
